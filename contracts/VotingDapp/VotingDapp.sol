566681f1d580747fe7ab6746d9a8f456c92daecf
ReyesBTC <christianreyesbtc@proton.me>
2023-05-25 20:56:34 +0200

    VotingDapp
    Started writing voting dApp.
    main functions, mappings, events, some modifiers.

---
+++ contracts/VotingDapp/VotingDapp.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9; 
+
+contract VotingDapp {
+
+address chairperson;
+bool electionOpen;
+string[] public listOfCandidates;
+
+//Events 
+  event PollsOpened();
+  event HasVoted(address indexed voter);
+  event CandidateAdded(address indexed candidate, string name);
+  event ElectionClosed();
+
+//Modifiers 
+modifier onlychairperson() {
+  require( msg.sender == chairperson, "you need to be the owner."); //Will require that the function caller be the owner of the contract. 
+  _;
+}
+
+modifier onlyWhenPollOpen() {
+  require(electionsOpen, "Elections are not currently open"); //Will require that the Elections are open. 
+  _;
+}
+
+modifier onlyWhenPollClosed() {
+  require(electionOpen = false, "Election is open"); //Will require that polls are closed
+  _;
+}
+
+constructor() {
+  chairperson = msg.sender;
+  electionsOpen = false;
+}
+
+//Mappings 
+mapping(address => uint256) public votes; //counts votes per address.
+mapping(address => bool) public hasVoted; //to make sure voters only cast one vote. 
+mapping(address => string) public candidates; //address to names.
+mapping(string => bool) private isCandidateNameRegistered; //avoid double registration. 
+
+  function addCandidate(string memory candidateName, address candidateId) public onlychairperson {
+    // require(candidates[candidateId] == candidateName, "Candidate has already been registered");
+    require(!isCandidateNameRegistered[candidateName], "This name has already been registered as a candidate.");
+    listOfCandidates.push(CandidateName);
+    candidates[candidateId] = candidateName;
+    isCandidateNameRegistered[candidateName] = true;
+    emit CandidateAdded(candidateId, candidateName);
+  }
+
+  function vote(address voteFor) public onlyDuringElections {
+    require(hasvoted[msg.sender] = false, "You have already voted");
+    votes[voteFor] ++,
+  }
+
+  function openElection() public onlyWhenPollClosed {
+    require(electionOpen = false, "Election is already open");
+    electionOpen = true;
+  }
+
+  function closeElection() public onlyWhenPollOpen onlyOwner {
+    electionOpen = false;
+  }
+
+  function countVotes() public onlyWhenPollOpen {
+
+  }
+
+}
